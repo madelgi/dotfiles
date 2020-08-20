2381cf6dd2eb7eb0714633e9fa75b056594a5804
Max Del Giudice <maxdelgiudice@gmail.com>
2020-08-20 03:18:13 +0000

    add small install packages script

---
+++ install_packages.sh
@@ -0,0 +1,28 @@
+####################################################################################################
+# Ubuntu install script
+####################################################################################################
+
+# Zsh
+sudo apt-get install zsh
+
+# Python packages
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+# Neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+
+# Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+
+# Oh My Zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Node
+curl -sL install-node.now.sh/lts | bash
